51794c1b37d0663ad37db46d5c6f4f07a119cf7c
Shailesh <50418172+sendtoshailesh@users.noreply.github.com>
2022-08-06 21:05:06 +0530

    Update cloud-sql-query-insight.sql

--- cloud-sql-query-insight.sql
+++ cloud-sql-query-insight.sql
@@ -33,17 +33,89 @@ CREATE TABLE Towns (
   UNIQUE (code, department)
 );
 
-insert into towns (
-    code, article, name, department
-)
+To insert one million rows into Towns
+
+
+insert into towns (code, article, name, department)
 select
     left(md5(i::text), 10),
     md5(random()::text),
     md5(random()::text),
     left(md5(random()::text), 4)
-from generate_series(1, 1000000) s(i)
+from generate_series(1, 1000000) s(i);
+
+CREATE TABLE Towns (
+  id SERIAL UNIQUE NOT NULL,
+  code VARCHAR(10) NOT NULL,
+  article TEXT,
+  name TEXT NOT NULL, -- not unique
+  department integer NOT NULL
+);
+
+insert into towns (code, article, name, department)
+select
+    left(md5(i::text), 10),
+    md5(random()::text),
+    md5(random()::text),
+    round(random()*1000)
+from generate_series(1, 1000000) s(i);
+
+\timing on
+select * from towns where department in (select distinct department from towns where department between 150 and 250);
+
+explain select * from towns where department in (select distinct department from towns where department between 150 and 250);
+
+pgbench --host=10.45.193.230 --username=postgres -n -c 2 -j 1 -S -T 9000 -f exp.sql  testdb
+
+create index idx_towns_dept on towns(department);
+
+Since id is a serial it is not necessary to include it.
 
 
+testdb=> \timing on
+Timing is on.
+testdb=> select * from towns where department in (select department from towns where department between 150 and 250);
+Time: 378.372 ms
+testdb=> explain select * from towns where department in (select distinct department from towns where department between 150 and 250);
+                                      QUERY PLAN
+---------------------------------------------------------------------------------------
+ Hash Join  (cost=29565.73..56487.85 rows=1000000 width=85)
+   Hash Cond: (towns.department = towns_1.department)
+   ->  Seq Scan on towns  (cost=0.00..24286.00 rows=1000000 width=85)
+   ->  Hash  (cost=29553.22..29553.22 rows=1001 width=4)
+         ->  HashAggregate  (cost=29533.20..29543.21 rows=1001 width=4)
+               Group Key: towns_1.department
+               ->  Seq Scan on towns towns_1  (cost=0.00..29286.00 rows=98880 width=4)
+                     Filter: ((department >= 150) AND (department <= 250))
+(8 rows)
+
+Time: 2.132 ms
+testdb=> select * from towns where department in (select distinct department from towns where department between 150 and 250);
+Time: 384.005 ms
+testdb=>
+testdb=>
+testdb=>
+testdb=>
+testdb=> create index idx_towns_dept on towns(department);
+CREATE INDEX
+Time: 1014.358 ms (00:01.014)
+testdb=> explain select * from towns where department in (select distinct department from towns where department between 150 and 250);
+                                              QUERY PLAN
+------------------------------------------------------------------------------------------------------
+ Hash Join  (cost=18150.88..45072.99 rows=1000000 width=85)
+   Hash Cond: (towns.department = towns_1.department)
+   ->  Seq Scan on towns  (cost=0.00..24286.00 rows=1000000 width=85)
+   ->  Hash  (cost=18138.36..18138.36 rows=1001 width=4)
+         ->  HashAggregate  (cost=18118.35..18128.35 rows=1001 width=4)
+               Group Key: towns_1.department
+               ->  Bitmap Heap Scan on towns towns_1  (cost=2101.95..17871.15 rows=98880 width=4)
+                     Recheck Cond: ((department >= 150) AND (department <= 250))
+                     ->  Bitmap Index Scan on idx_towns_dept  (cost=0.00..2077.23 rows=98880 width=0)
+                           Index Cond: ((department >= 150) AND (department <= 250))
+(10 rows)
+
+Time: 1.586 ms
+testdb=>
 
 
 ===============================================================================================
