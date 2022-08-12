b59516c1cda08a7d50f3a1729b40c6bdf2566007
Shailesh <50418172+sendtoshailesh@users.noreply.github.com>
2022-08-12 19:42:05 +0530

    Create cloud-sql-gcloud-commands.sh

---
+++ cloud-sql-gcloud-commands.sh
@@ -0,0 +1,8 @@
+
+gcloud sql instances list
+
+
+
+gcloud sql instances create prod-instance --database-version=POSTGRES_9_6 --cpu=2 --memory=8GiB --zone=us-central1-a --root-password=password123
+
+gcloud sql instances create pg-db22 --database-version=POSTGRES_10 --cpu=4 --memory=16GiB --zone=asia-south1-a --root-password=Welcome0
