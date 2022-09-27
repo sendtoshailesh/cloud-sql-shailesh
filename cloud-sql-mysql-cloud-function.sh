84b54cb3cdaa7d94f6d61d7c12811e7efd09e357
Shailesh <50418172+sendtoshailesh@users.noreply.github.com>
2022-09-27 19:51:19 +0530

    Update cloud-sql-mysql-cloud-function.sh

--- cloud-sql-mysql-cloud-function.sh
+++ cloud-sql-mysql-cloud-function.sh
@@ -7,7 +7,20 @@ https://cloud.google.com/sql/docs/postgres/connect-functions#private-ip_1
 
 
 
+cat requirements.txt
 
+cat main.py
+
+in same dir where main.py and requirements.txt exist, run following to deploy the cloud function along with the code:
+
+gcloud beta functions deploy sql_connect --runtime python37 --project shailesh-1 --region asia-south1 --vpc-connector serverless-1 --trigger-http --service-account 664290125703-compute@developer.gserviceaccount.com
+
+Test with following format:
+
+curl -m 70 -X POST https://asia-south1-shailesh-1.cloudfunctions.net/sql_connect \
+-H "Authorization: bearer $(gcloud auth print-identity-token)" \
+-H "Content-Type: application/json" \
+-d '{}'
 
 
 
@@ -56,5 +69,17 @@ versionId: '5'
 vpcConnector: serverless-1
 vpcConnectorEgressSettings: PRIVATE_RANGES_ONLY
 admin_shkm_altostrat_com@instance-mumbai:~/bigtable$
+
+admin_shkm_altostrat_com@instance-mumbai:~/bigtable/pg$ curl -m 70 -X POST https://asia-south1-shailesh-1.cloudfunctions.net/sql_connect \
+> -H "Authorization: bearer $(gcloud auth print-identity-token)" \
+> -H "Content-Type: application/json" \
+> -d '{}'
+['audit_log_rules', 'audit_log_rules_expanded', 'audit_log_supported_ops', 'cloudsql_replica_index', 'columns_priv', 'component', 'db', 'default_roles', 'engine_cost', 'func', 'general_log', 'global_grants', 'gtid_executed', 'heartbeat', 'help_category', 'help_keyword', 'help_relation', 'help_topic', 'innodb_index_stats', 'innodb_table_stats', 'password_history', 'plugin', 'procs_priv', 'proxies_priv', 'replication_asynchronous_connection_failover', 'replication_asynchronous_connection_failover_managed', 'replication_group_configuration_version', 'replication_group_member_actions', 'role_edges', 'server_cost', 'servers', 'slave_master_info', 'slave_relay_log_info', 'slave_worker_info', 'slow_log', 'tables_priv', 'time_zone', 'time_zone_leap_second', 'time_zone_name', 'time_zone_transition', 'time_zone_transition_type', 'user']admin_shkm_altostrat_com@instance-mumbai:~/bigtable/pg$
+admin_shkm_altostrat_com@instance-mumbai:~/bigtable/pg$
+admin_shkm_altostrat_com@instance-mumbai:~/bigtable/pg$
+admin_shkm_altostrat_com@instance-mumbai:~/bigtable/pg$
+
+
+
 
 
